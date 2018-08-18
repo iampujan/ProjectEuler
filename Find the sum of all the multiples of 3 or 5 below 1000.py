b5b9b388448c81fb0fed158f78a87b3699a42cc3
Pujan Thapa <iampujan@outlook.com>
2018-08-18 08:56:37 +0545

    little modified

--- Find the sum of all the multiples of 3 or 5 below 1000.py
+++ Find the sum of all the multiples of 3 or 5 below 1000.py
@@ -5,13 +5,8 @@ Created on Sat Aug 18 08:20:24 2018
 
 @author: pujan
 """
-
-
-
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
-
-
 
 target = 1000
 sum = 0
